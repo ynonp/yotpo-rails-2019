760461200b414b47f3184e5ec93ec2b83f0bef05
ynonp <ynonperek@gmail.com>
2019-07-10 09:55:52 +0300

    ADD functionality to test

--- 02-architecture/app/models/tag.rb
+++ 02-architecture/app/models/tag.rb
@@ -1,4 +1,14 @@
 class Tag < ApplicationRecord
   has_many :tag_tickets
   has_many :tickets, through: :tag_tickets
+  after_save :sync_with_remote_api
+
+  def important?
+    name.end_with?('!')
+  end
+
+  def sync_with_remote_api
+    puts "syncing the tag #{name} with a remote API.."
+    sleep 3
+  end
 end
